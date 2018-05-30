051f5af26f030ba18b81585ce4fb5a3dd07d94f2
totorototo <el.totorototo@gmail.com>
2018-05-30 19:34:29 +0200

    update component

--- src/components/swatch/Swatch.js
+++ src/components/swatch/Swatch.js
@@ -1,26 +1,62 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-primitives";
+import PropTypes from "prop-types";
 import styled from "styled-components/primitives";
+import chroma from "chroma-js";
 
-const Wrapper = styled.View`
-  backgroundcolor-color: papayawhip;
-  flex: 1;
+// take a hex and give us a nice text color to put over it
+const textColor = hex => {
+  const vsWhite = chroma.contrast(hex, "white");
+  if (vsWhite > 4) {
+    return "#FFF";
+  }
+  return chroma(hex)
+    .darken(3)
+    .hex();
+};
+
+const SwatchTile = styled.View`
+  height: 250px;
+  width: 250px;
+  border-radius: 4px;
+  margin: 4px;
+  background-color: ${props => props.hex};
   justify-content: center;
   align-items: center;
 `;
 
-const Title = styled.Text`
-  font-size: 20;
-  text-align: center;
-  margin: 10;
-  color: palevioletred;
+const SwatchName = styled.Text`
+  color: ${props => textColor(props.hex)};
   font-weight: bold;
 `;
 
-const Swatch = () => (
-  <Wrapper>
-    <Title>Hello world, this is my first universal styled component</Title>
-  </Wrapper>
+const Ampersand = styled.Text`
+  color: #f3f3f3;
+  font-size: 120px;
+  font-family: Himalaya;
+  line-height: 144px;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-family: "GT America";
+  font-weight: bold;
+  padding: 4px;
+`;
+
+const Swatch = ({ name, hex }) => (
+  <SwatchTile name={`Swatch ${name}`} hex={hex}>
+    <SwatchName name="Swatch Name" hex={hex}>
+      {name}
+    </SwatchName>
+    <Ampersand hex={hex}>&</Ampersand>
+  </SwatchTile>
 );
+
+const Color = {
+  hex: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+};
+
+Swatch.propTypes = Color;
 
 export default Swatch;
